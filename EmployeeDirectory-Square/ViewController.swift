62a3ec8f2c02c6173d5ad3edb6aae91964695dc2
Li Zhao <zhaoli841211@gmail.com>
2022-08-19 11:00:21 -0700

    Add files via upload

---
+++ EmployeeDirectory-Square/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmployeeDirectory-Square
+//
+//  Created by Zhao Li on 8/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
