e34af7604df1bfe04ae5712568581767c27c61ea
Li Zhao <zhaoli841211@gmail.com>
2022-08-19 13:47:02 -0700

    Create employee data model
    Create employee data model

---
+++ EmployeeDirectory-Square/EmployeeDataModel.swift
@@ -0,0 +1,19 @@
+//
+//  EmployeeDataModel.swift
+//  EmployeeDirectory-Square
+//
+//  Created by Zhao Li on 8/19/22.
+//
+
+import Foundation
+
+struct EmployeeDataModel {
+  var name : String
+  var imageSmallUrlString : String
+  var imageLargeUrlString : String
+  var phonenNumber : String
+  var email : String
+  var biography : String
+  var team : String
+  var type : String
+}
